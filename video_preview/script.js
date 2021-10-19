2e0979f2989d9a968684eb8706a347e67867cb28
jrduncan1 <dunc5201@gmail.com>
2021-10-19 09:27:56 -0600

    commit video preview assignment

---
+++ video_preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function over(element){
+    var c = document.getElementById("myVideo");
+    c.controls = true;
+}
+
+function out(element){
+    var c = document.getElementById("myVideo");
+    c.controls = false;
+}
